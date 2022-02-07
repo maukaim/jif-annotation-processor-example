a5c2fd66b2d9eeac67d628de07ac8e6e6375b52d
Julien Elkaim <julienelk@gmail.com>
2022-02-07 17:12:02 +0100

    Created 2 modules, 1 for annotation Processor work, 1 for core project example

---
+++ example-core/src/main/java/org/maukaim/jif/example/core/MainApplication.java
@@ -0,0 +1,12 @@
+package org.maukaim.jif.example.core;
+
+
+import org.maukaim.jif.annotation.processor.HelloWorldSentence;
+
+public class MainApplication {
+
+    public static void main (String[] args){
+        System.out.println(HelloWorldSentence.ENGLISH);
+
+    }
+}
